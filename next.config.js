a769db59cecdd5e6d0031404d05897e318666a63
Sternblitz <info@sternblitz.de>
2025-10-15 14:35:53 +0200

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: true }
-};
-export default nextConfig;
+const nextConfig = {};
+module.exports = nextConfig;
