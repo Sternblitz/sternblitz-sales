3d2ff9cf275848c916f2ce2885137eecf41996ca
Sternblitz <info@sternblitz.de>
2025-10-15 23:07:54 +0200

    Update page.jsx

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useMemo, useState } from "react";
-import LiveSimulator from "@/components/live-simulator"; // <- dein Pfad
+import LiveSimulator from "@/components/livesimulator"; // <- dein Pfad
 
 export default function DashboardPage() {
   // --- UI / Offen-States ---
